287b664017fbcdff26ea2652d59e339e1dde462c
tsiresy <44996935+rabekototsiresy@users.noreply.github.com>
2024-08-13 14:32:19 +0300

    fix: refactor get pilotage

--- src/modules/pilotage/getPilotageController.ts
+++ src/modules/pilotage/getPilotageController.ts
@@ -37,56 +37,24 @@ export const getPilotageController = async (
             (file: any) => ({ ...file, pilotageId: pilotage.id, status: 2 }) //0: ko, 1: ok, 2: en cours
           );
           await FileModel.bulkCreate(fileList);
-
-          // }
-          /**
-           * send request to planetpress
-           */
-
-          const pageAsNumber = Number.parseInt((req.query as any).page);
-          const sizeAsNumber = Number.parseInt((req.query as any).size);
-
-          let page = 0;
-
-          if (!Number.isNaN(pageAsNumber) && pageAsNumber > 0) {
-            page = pageAsNumber;
-          }
-          let size = 5;
-          if (!Number.isNaN(sizeAsNumber)) {
-            size = sizeAsNumber;
-          }
-
-          // let whereClause = {} as any;
-
-          // if (filename) {
-          //   whereClause.filename = { [Op.like]: `%${filename}%` }; // For partial matches
-          // }
-
-          // if (createdAt) {
-          //   whereClause.createdAt = createdAt;
-          // }
-
-          // if (status !== undefined) {
-          //   whereClause.status = status;
-          // }
-
-          const pilotageList = await PilotageModel.findAndCountAll({
-            attributes: ["id", "filename", "status", "createdAt"],
-            // where: whereClause,
-            // limit: size,
-            // offset: page * size
-          });
-          return ApiResponse(
-            res,
-            SUCCESS_CODE_200,
-            true,
-            "List of all pilotage",
-            {
-              records: pilotageList.rows,
-              count: pilotageList.count,
-            }
-          );
         }
+
+        const pilotageList = await PilotageModel.findAndCountAll({
+          attributes: ["id", "filename", "status", "createdAt"],
+          // where: whereClause,
+          // limit: size,
+          // offset: page * size
+        });
+        return ApiResponse(
+          res,
+          SUCCESS_CODE_200,
+          true,
+          "List of all pilotage",
+          {
+            records: pilotageList.rows,
+            count: pilotageList.count,
+          }
+        );
       } else {
         const pilotageList = await PilotageModel.findAndCountAll({
           attributes: ["id", "filename", "status", "createdAt"],
